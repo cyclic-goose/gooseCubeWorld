15b245ab42c5aff068007bb83c8c2a59c4d8bdfd
Brenden <bstevens2225@gmail.com>
2026-01-13 20:51:37 -0800

    build and launch json, includes, c++ compile test

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+
+int main()
+{
+
+    std::cout << "C++ Compile Test" << std::endl;
+    return 0;
+
+}
