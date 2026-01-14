278bcb5173e2fe158d27d8e1cb2a0c56a2908b0f
Brenden <bstevens2225@gmail.com>
2026-01-13 21:54:36 -0800

    default width and height

--- include/constants.h
+++ include/constants.h
@@ -4,6 +4,8 @@
 struct GeneralSettings{
     const int OPEN_GL_CORE_MAJOR = 4;
     const int OPEN_GL_CORE_MINOR = 6;
+    const int START_WINDOW_WIDTH = 1920;
+    const int START_WINDOW_HEIGHT = 1080;
 
 };
 
